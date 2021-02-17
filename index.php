947d2787aeba4fc71b169ad18a90c668485b9673
Miguel <miguel.diazv84@gmail.com>
2021-02-17 16:47:20 +0100

    Se ha finalizado y creado el index.

--- index.php
+++ index.php
@@ -12,6 +12,35 @@ $connect = mysqli_connect($host, $user, $password, $database);
 if(mysqli_connect_error()){
     echo "La conexión a la base de datos a fallado:".mysqli_connect_error();
 
+} else{
+    echo "Conexion realizada a la base de datos".'<br/><br/>';
+}
+
+// Consulta para configurar la codificación de caracteres.
+$sql = "SET NAMES 'utf8'";
+mysqli_query($connect, $sql);
+
+// Consulta SELECT desde php
+$sql = "SELECT * FROM notas";
+$query = mysqli_query($connect, $sql);
+
+
+while($nota = mysqli_fetch_assoc($query)){
+    // var_dump($nota);
+    echo "<h2>".$nota ['titulo']."</h2>";
+    echo $nota ['descripcion'].'<br/>';
+    echo $nota ['color'].'<br/>';
+}
+
+// Insertar datos en la base de datos desde PHP
+$sql = "INSERT INTO notas VALUES (null, 'Nota creada desde php', 'Esto es una PHP', 'green')";
+$insert = mysqli_query($connect, $sql);
+
+if($insert){
+    echo "Datos insertados correctamente";
+
+}else{
+    echo "Error: ".mysqli_error($connect);
 }
 
 
