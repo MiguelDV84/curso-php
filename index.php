e8918421cf1bf43e4e7082bae398552b76eca1a2
Miguel <miguel.diazv84@gmail.com>
2021-02-17 16:13:50 +0100

    Probando conexion de base de datos

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ $database = 'phpmysql';
 
 $connect = mysqli_connect($host, $user, $password, $database);
 
+// Comprobar que la conexion es correcta
+if(mysqli_connect_error()){
+    echo "La conexión a la base de datos a fallado:".mysqli_connect_error();
+
+}
+
 
         
 ?>
